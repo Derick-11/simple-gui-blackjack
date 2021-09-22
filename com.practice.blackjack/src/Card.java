db30282ce8014e550efc9770e646f56466dcd7b8
Derick C <derick.a.cornejo@gmail.com>
2021-09-21 17:28:56 -0700

    Set up and imported assets for cards

--- com.practice.blackjack/src/Card.java
+++ com.practice.blackjack/src/Card.java
@@ -2,11 +2,18 @@ public class Card {
     private String suit;
     private String value;
     private int score;
+    private String imageFile;
 
     public Card(String suit, String value, int score) {
         this.suit = suit;
         this.value = value;
         this.score = score;
+    }
+    public Card(String suit, String value, int score, String file) {
+        this.suit = suit;
+        this.value = value;
+        this.score = score;
+        this.imageFile = file;
     }
 
     public String getSuit() {
@@ -18,4 +25,6 @@ public class Card {
     public int getScore() { return this.score; }
 
     public void setScore(int score) { this.score = score; }
+
+    public String getImageFile() { return this.imageFile; }
 }
